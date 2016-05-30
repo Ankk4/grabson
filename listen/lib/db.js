b784cdfffcd43f2ef1fcb722174e4229adf648db
administrator <administrator@debian-auto.unassigned-domain>
2016-05-30 20:40:47 +0300

    get ready

--- listen/lib/db.js
+++ listen/lib/db.js
@@ -1,9 +1,8 @@
 var mongoose   = require('mongoose');
 var Schema     = mongoose.Schema();
 
-var DataModel = require('../models/dataModel');
-mongoose.connect('mongodb://localhost/gameData');
-
+// Fake sync connection, no need to worry about first connecting
+mongoose.connect('mongodb://localhost/gamedata');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Database connection error:'));
 db.once('open', function() {
@@ -11,26 +10,36 @@ db.once('open', function() {
   // we're connected!
 });
 
-var InsertData = function(gameData) {
+var InsertData = function(model, instance) {
 	// new data instance created with data model and then saved
-	var data = new DataModel(gameData);
+	var data = new Model(instance);
 	data.save(function (err, obj) {
 		if (err) return console.error(err);
 	});
 };
 
-var QueryData = function(query) {
-	DataModel.find(query, function(err, data){
+var QueryData = function(model, query) {
+	model.find(query, function(err, data){
 		if (err) return console.error(err);
 		return data;
 	});
 };
 
-var GetById = function(id) {
-	DataModel.findById(id, function(err, data){
-		if (err) return console.error(err);
-		return data;
-	});
+var GetById = function(model, id) {
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		console.log("ObjectID is not valid: ", id);
+		return null;
+	}
+	else {
+		model.findById(id, function(err, data){
+			if (err) {
+				console.error("Something happend while working with db: ", err);
+				return null;
+			}
+			else 
+				return data;
+		});
+	}
 };
 
 module.exports.InsertGameData = InsertData;
--- listen/lib/routes/dataRouter.js
+++ listen/lib/routes/dataRouter.js
@@ -1,18 +1,35 @@
 var db = require('../db');
 
+// Models
+var Data = require('../../models/dataModel');
+
 module.exports = (function() {
     'use strict';
     var router = require('express').Router({mergeParams: true});
 
+    //Get for querying with ?key=value
     router.get('/', function(req, res) {
         var query = req.query;
-        var result = db.QueryData(query);
+        console.log("Query for: " + JSON.stringify(query));        
+        var result = db.QueryData(DataModel, query);
         res.json(result);
     });
 
+    //Get for getting single data by id games/id
     router.get('/:gameId', function(req, res) {
-        var result = db.GetById(req.params.id);
-        res.json(result);
+        console.log("Query by id: " + req.params.gameId);
+        var result = db.GetById(DataModel, req.params.gameId);
+        if(result !== null) {
+            //Success
+            res.json(result);
+        } else {
+            res.status(500).send({ error: 'Something failed!' });
+        }        
+    });
+
+    router.post('', function(req, res) {
+        var data = new Data();
+
     });
 
     return router;
