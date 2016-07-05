b2dfc15e44f9af67f70d2f64fb9b2eed944ac6fc
Ankk4 <antero.juutinen@gmail.com>
2016-07-05 13:22:25 +0300

    env not available in tmux

--- models/game.js
+++ models/game.js
@@ -11,3 +11,4 @@ var gameModel = new Schema({
 });
 
 module.exports = mongoose.model('Game', gameModel);
+
--- models/user.js
+++ models/user.js
@@ -5,6 +5,10 @@ var Schema    = mongoose.Schema;
 // Problem - Random chance to fail with unique apikey
 // Solution - pre save function that checks and generates new one if nessesery
 
+/*GameData:[
+	{ type: Schema.Types.ObjectId, ref: 'Game' }
+]*/
+
 // Schema - User
 var userModel = new Schema({
 	username:		{ type: String, unique: true, required: true},
